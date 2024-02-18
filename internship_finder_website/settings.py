ce326ff85bddcdecb0bf3a318f20a4ef4e613eac
Kamesh Fernando <kameshkumara921@gmail.com>
2024-02-18 11:08:30 +0530

    create models

--- internship_finder_website/settings.py
+++ internship_finder_website/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'widget_tweaks'
+    'widget_tweaks',
+    'resume',
+    'company',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL='users.User'
+AUTHENTICATION_BACKENDS=['users.backends.EmailBackend']
